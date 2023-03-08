c3925c69c73e5c2372b6153be0c10bf9ead091eb
triviere19 <triviere19@comcast.net>
2023-03-08 00:28:19 -0500

    set bg hopefully

---
+++ js/main.js
@@ -0,0 +1,52 @@
+//DOM Elements
+
+const time = document.getElementById('time'),
+    greeting = document.getElementById('greeting'),
+    greeting = document.getElementById('name');
+
+//Show Time
+function showTime(){
+    let today = new Date(),
+        hour = today.getHours(),
+        min = today.getMinutes(),
+        sec = today.getSeconds();
+
+    //Set AM or PM
+    const amPm = hour >= 12 ? 'PM' : 'AM';
+
+    //12hr Format
+    hour = hour % 12 || 12;
+
+    //Output time
+    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+
+    setTimeout(showTime, 1000);
+}
+
+
+function addZero(n){
+    return (parseInt(n, 10) < 10 ? '0' : '')
+}
+
+
+function setBg(){
+    let today = new Date(),
+        hour = today.getHours();
+
+    if (hour < 12){
+        document.body.style.backgroundImage = "url('upside-down-emoji.jpg')"
+        greeting.textContext = 'Ohayo';
+    }
+    else if (hour<18){
+        document.body.style.backgroundImage = "url('upside-down-emoji.jpg')"
+        greeting.textContext = 'Oyasumi';
+    }
+    else {
+        document.body.style.backgroundImage = "url('upside-down-emoji.jpg')"
+        greeting.textContext = 'Konnichiwa';
+    }
+}
+
+//Run
+showTime();
+setBG();
