cb72dc30088f9219e1eb98da2f5f5388e4b23f9f
triviere19 <triviere19@comcast.net>
2023-03-23 23:53:54 -0400

    more pagezzzzzz bug fixes

--- js/main.js
+++ js/main.js
@@ -183,6 +183,6 @@ function main(){
 //    getSongOTD();
 
     //scroll to top of page
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+//    document.body.scrollTop = 0; // For Safari
+//    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
